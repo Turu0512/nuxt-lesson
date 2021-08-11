ee15e7faeced0814804e215b3d131927e13bf1df
Turu0512 <hinataptyan@gmail.com>
2021-08-11 20:02:07 +0900

    axiosによるデータの取得、エラーメッセージまで

---
+++ axios-lesson/pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class="container">
+    <div>
+      <ul>
+        <li v-for="user in users" :key="user.id">
+          {{ user.id }},{{ user.name }}-{{user.company.name}}
+        </li>
+      </ul>
+    </div>
+  </section>
+</template>
+
+<script>
+const axios = require('axios');
+let url = 'https://jsonplaceholder.typicode.com/users'
+
+export default {
+asyncData({ params , error }){
+  return axios.get(url)
+    .then((res) => {
+      return { users: res.data }
+    })
+    .catch((e) => {
+      error({ statusCode: e.response.status, message: e.message })
+  })
+
+}}
+</script>
