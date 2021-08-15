2ca62ce930a6b0c178a957dbb7d770406a7d8db7
Turu0512 <hinataptyan@gmail.com>
2021-08-15 13:55:44 +0900

    追加、削除の実装

--- todo-note/pages/index.vue
+++ todo-note/pages/index.vue
@@ -1,10 +1,19 @@
 <template>
   <div class="page">
     <ul>
-      <li v-for="todo in todos" :key="todo">
+      <li v-for="(todo, index) in todos" 
+      :key="todo"
+      @click='deleteTodo(index)'>
         {{ todo }}
       </li>
     </ul>
+    <form @submit.prevent="submitTodo">
+      <input 
+      v-model="todo"
+      type="text"
+      placeholder="Add a Todo">
+      <button type="submit">Add Todo</button>
+    </form>
   </div>
 </template>
 
@@ -12,7 +21,20 @@
 export default {
   data() {
     return {
-      todos:['これと','あれと','それ']
+      todos:['これと','あれと','それ'],
+      todo:''
+    }
+    
+  },
+  methods: {
+    submitTodo(){
+      if(this.todo){
+        this.todos.push(this.todo)
+        this.todo=''
+      }
+    },
+    deleteTodo(index){
+      this.todos.splice(index,1)
     }
   }
 }
