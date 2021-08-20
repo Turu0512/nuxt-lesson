97e6918a990cc91c8a76f91fce226f222ec52f3a
Turu0512 <hinataptyan@gmail.com>
2021-08-20 18:48:43 +0900

    データの送信

--- nuxt-fb/plugins/firebase.js
+++ nuxt-fb/plugins/firebase.js
@@ -3,13 +3,13 @@ import firebase from 'firebase'
 if (!firebase.apps.length) {
   firebase.initializeApp(
     {
-     apiKey: "AIzaSyAG9rj0maLaMs9fPiwaED_v-QxI3dalJiI",
-     authDomain: "nuxt-data-cab99.firebaseapp.com",
-     projectId: "nuxt-data-cab99",
-     storageBucket: "nuxt-data-cab99.appspot.com",
-     messagingSenderId: "998280629985",
-     appId: "1:998280629985:web:e8d534b3164fb2021631a0",
-     measurementId: "G-K4XQWNFCLV"
+    apiKey: "AIzaSyAG9rj0maLaMs9fPiwaED_v-QxI3dalJiI",
+    authDomain: "nuxt-data-cab99.firebaseapp.com",
+    projectId: "nuxt-data-cab99",
+    storageBucket: "nuxt-data-cab99.appspot.com",
+    messagingSenderId: "998280629985",
+    appId: "1:998280629985:web:e8d534b3164fb2021631a0",
+    measurementId: "G-K4XQWNFCLV"
     }
   )
 }
